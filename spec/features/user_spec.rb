19c323191da2630f7d506dc2c450ccd03f503605
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-03-28 15:30:19 -0600

    completes through login without authorization

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,6 +6,27 @@ RSpec.describe 'As a visitor' do
 
     click_link "Register"
 
+    fill_in "user[name]", with: "test_user"
+    fill_in "user[email]", with: "tu@test.com"
+    fill_in "user[password]", with: "sonsbirhday"
 
+    click_on "Create User"
+
+    expect(current_path).to eq(root_path)
+    expect(User.last.name).to eq("test_user")
+  end
+
+  it 'can log in' do
+    visit root_path
+
+    click_link "Login"
+
+    fill_in "email", with: "tu@test.com"
+    fill_in "password", with: "sonsbirhday"
+
+    click_on "Login"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("You have successfully logged in!")
   end
 end
